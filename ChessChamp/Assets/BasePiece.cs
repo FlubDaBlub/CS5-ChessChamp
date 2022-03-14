d48d01c4633ef8fd9746b0cfb19acaf8eb32bce0
Colton Stemler <colton1914@gmail.com>
2022-03-14 09:24:42 -0700

    Semi-Functional Movement

--- ChessChamp/Assets/BasePiece.cs
+++ ChessChamp/Assets/BasePiece.cs
@@ -37,4 +37,76 @@ public abstract class BasePiece : EventTrigger
       transform.position = newCell.transform.position;
       gameObject.SetActive(true);
     }
+
+    private void CreateCellPath(int xDirection, int yDirection, int movement) {
+      // Target position
+      int currentX = mCurrentCell.mBoardPosition.x;
+      int currentY = mCurrentCell.mBoardPosition.y;
+
+      //Check each cell
+      for(int i = 1; i <= movement; i++) {
+        currentX += xDirection;
+        currentY += yDirection;
+
+        // TODO: Get the state of the target cell
+
+        // Add to list
+        mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[currentX, currentY]);
+      }
+    }
+
+    protected virtual void CheckPathing() {
+      // Horizontal
+      CreateCellPath(1, 0, mMovement.x);
+      CreateCellPath(-1, 0, mMovement.x);
+
+      // Vertical
+      CreateCellPath(0, 1, mMovement.y);
+      CreateCellPath(0, -1, mMovement.y);
+
+
+      // Upper diagonal
+      CreateCellPath(1, 1, mMovement.z);
+      CreateCellPath(-1, 1, mMovement.z);
+
+      // Lower diagonal
+      CreateCellPath(-1, -1, mMovement.z);
+      CreateCellPath(1, -1, mMovement.z);
+    }
+
+    protected void ShowCells() {
+      foreach(Cell cell in mHighlightedCells)
+        cell.mOutlineImage.enabled = true;
+    }
+
+    protected void ClearCells() {
+      foreach(Cell cell in mHighlightedCells)
+        cell.mOutlineImage.enabled = false;
+
+      mHighlightedCells.Clear();
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData) {
+      base.OnBeginDrag(eventData);
+
+      // Test for cells
+      CheckPathing();
+
+      // Show valid cells
+      ShowCells();
+    }
+
+    public override void OnDrag(PointerEventData eventData) {
+      base.OnDrag(eventData);
+
+      // Follow pointer
+      transform.position += (Vector3)eventData.delta;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData) {
+      base.OnEndDrag(eventData);
+
+      // Hide
+      ClearCells();
+    }
 }
