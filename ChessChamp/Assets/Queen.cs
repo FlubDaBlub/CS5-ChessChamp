0c3d45aadfb123ef0be0e42e8b688bb16aac4a1b
Colton Stemler <colton1914@gmail.com>
2022-04-22 08:43:54 -0700

    Queen and bishop movement

--- ChessChamp/Assets/Queen.cs
+++ ChessChamp/Assets/Queen.cs
@@ -8,6 +8,7 @@ public class Queen : BasePiece
   public override void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager) {
     base.Setup(newTeamColor, newSpriteColor, newPieceManager);
 
+    mMovement = new Vector3Int(7, 7, 7);
     GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Queen");
   }
 }
